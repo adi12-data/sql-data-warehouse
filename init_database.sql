db77451b1135c2d2a6cd3a278068e16381faf0dc
Adiii <khandelwaladi123@gmail.com>
2025-09-19 23:30:23 +0530

    Create init_database.sql

---
+++ init_database.sql
@@ -0,0 +1,14 @@
+-- Create Database 'DataWarehouse'
+-- Use master
+USE master;
+
+Create Database DataWarehouse;
+
+Use DataWarehouse;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+
